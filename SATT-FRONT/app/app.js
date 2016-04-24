9c5a014c1cd3537dc7a1bc8763f946cba5cdcb8a
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2016-04-24 17:40:47 -0500

    User Access con Tokens y Roles en FRONT Y BACK

--- SATT-FRONT/app/app.js
+++ SATT-FRONT/app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
+    'myApp.login',
     'myApp.eventos',
     'myApp.alertas',
     'myApp.nuevoEvento',
@@ -10,6 +11,13 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({
-        redirectTo: '/'
+        redirectTo: '/login'
     });
+}])
+.controller('IndexCtrl', ['$scope', '$localStorage', '$location', function ($scope, $localStorage, $location) {
+    $scope.$storage = $localStorage;
+    $scope.logOut = function () {
+        delete $localStorage.userInfo;
+        $location.path("/login");
+    };
 }]);
--- SATT-FRONT/app/modulos/eventos/eventos.js
+++ SATT-FRONT/app/modulos/eventos/eventos.js
@@ -9,15 +9,22 @@ angular.module('myApp.eventos', ['ngRoute'])
     });
 }])
 
-.controller('EventosCtrl', ['$scope', '$http', function ($scope, $http) {
+.controller('EventosCtrl', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
     $http({
         method: 'GET',
-//        url: 'http://uniandes-satt.herokuapp.com/eventos'
+        //        url: 'http://uniandes-satt.herokuapp.com/eventos'
         url: 'http://localhost:8010/eventos'
+        //url: 'http://localhost:8080/eventos',
+        headers: {
+            "Authorization": $localStorage.userInfo !== undefined ? $localStorage.userInfo.accessToken : null
+        }
     }).then(function successCallback(response) {
         $scope.eventos = response.data;
     }, function errorCallback(response) {
-        console.log("ER0ROOOROROOROROROR");
-        $scope.error = "Oh oh! Hubo un problema. Lo sentimos, intenta mas tarde :(";
+        if(response.status == 401) {
+            $scope.error = "Oh oh! Parece que requieres permisos que no te han otorgado :(";
+        } else {
+            $scope.error = "Oh oh! Hubo un problema. Lo sentimos, intenta mas tarde :(";   
+        }
     });
 }]);
